234188436311b87ed1536ac85606184dcaf2d130
anjinsan132131 <anjinsan132131@gmail.com>
2022-01-31 21:39:19 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.replace(/\*{10}/g, ' ').split(' ');
+
+    const newArr = [];
+
+    for (let i of expr) {
+        for (let j = 0; j < i.length; j += 10) {
+            let char = i.slice(j, j + 10).replace(/11/g, '-').replace(/10/g, '.').replace(/0/g, '');
+            newArr.push(MORSE_TABLE[char]);
+        }
+        newArr.push(' ');
+    }
+
+    return newArr.join('').slice(0, -1);
 }
 
 module.exports = {
